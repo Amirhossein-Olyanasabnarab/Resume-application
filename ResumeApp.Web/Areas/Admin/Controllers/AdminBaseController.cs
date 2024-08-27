8057d760fdc6c01e51420b77952b00c480d0ddbd
amirholya01 <amirholya01@gmail.com>
2024-08-27 21:39:39 +0200

    Admin Area

---
+++ ResumeApp.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeApp.Web.Areas.Admin.Controllers;
+[Area("Admin")]
+public class AdminBaseController : Controller
+{
+    protected string SuccessMessage = "SuccessMessage";
+    protected string ErrorMessage = "ErrorMessage";
+}
