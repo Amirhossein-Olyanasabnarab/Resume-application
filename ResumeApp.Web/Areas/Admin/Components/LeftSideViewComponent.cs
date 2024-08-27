ccc1556e39033da2e778c5221f05c7a141de3ec5
amirholya01 <amirholya01@gmail.com>
2024-08-27 22:28:17 +0200

    Admin Area ViewComponent-partial View

---
+++ ResumeApp.Web/Areas/Admin/Components/LeftSideViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeApp.Web.Areas.Admin.Components;
+
+public class LeftSideViewComponent : ViewComponent
+{
+    #region Methods
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("LeftSideBar");
+    }
+
+    #endregion
+}
